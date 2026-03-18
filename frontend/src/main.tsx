949d3650725b51c7b933f0ce79dc2ff480d78bcb
mrhoody <hud.syafiq@gmail.com>
2026-03-18 11:36:18 +0800

    induce build

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 
 const rootEl = document.getElementById("root");
-if (!rootEl) throw new Error("Root element #root not found");
+if (!rootEl) throw new Error("Root element #root not found!");
 
 createRoot(rootEl).render(
   <StrictMode>
